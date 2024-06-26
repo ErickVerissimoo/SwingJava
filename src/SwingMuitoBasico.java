52054874bba8f5e5373973301cdc8da7121bc873
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2024-06-26 20:28:36 -0300

    introdução ao swing no netbeans

---
+++ src/SwingMuitoBasico.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author gil
+ */
+import javax.swing.*;
+public class SwingMuitoBasico extends JFrame {
+    public void primeiraTela(){
+    setSize(100, 300);
+    setVisible(true);} 
+    public static void main (String [] args){
+    SwingMuitoBasico  a = new SwingMuitoBasico();
+    a.primeiraTela();
+    }
+}
+ 
