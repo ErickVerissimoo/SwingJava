20bfcdeda865e69ebca90a3cc99218b5268c7c71
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2024-06-26 21:50:32 -0300

    pequenas mudanças

--- src/SwingMuitoBasico.java
+++ src/SwingMuitoBasico.java
@@ -5,16 +5,17 @@
 
 /**
  *
- * @author gil
+ * @author Erick
  */
+import java.awt.*;
 import javax.swing.*;
-public class SwingMuitoBasico extends JFrame {
-    public void primeiraTela(){
-    setSize(100, 300);
-    setVisible(true);} 
+public class SwingMuitoBasico {
     public static void main (String [] args){
-    SwingMuitoBasico  a = new SwingMuitoBasico();
-    a.primeiraTela();
+        JFrame tela = new JFrame();
+        tela.setBackground(Color.black);
+        tela.setTitle("primeira tela em Swing");
+        tela.setSize(400, 400);
+        tela.setResizable(true);
+                tela.setVisible(true);
     }
 }
- 
